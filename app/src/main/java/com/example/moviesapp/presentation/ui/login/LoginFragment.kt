198064e28570d66eec25e2fdde4e273f271377fb
Jersson Melanio Diaz Santa Cruz <jerson.diaz.15@gmail.com>
2022-04-02 12:58:11 -0500

    [WIP] Modify fragments and layouts

--- app/src/main/java/com/example/moviesapp/presentation/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/moviesapp/presentation/ui/login/LoginFragment.kt
@@ -1,7 +1,35 @@
 package com.example.moviesapp.presentation.ui.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.moviesapp.R
+import com.example.moviesapp.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginFragment {
+class LoginFragment: Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        initListener()
+        return binding.root
+    }
+
+    private fun initListener() {
+        binding.iFlCard.bClSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_movie)
+        }
+    }
+
 }
